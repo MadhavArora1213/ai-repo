5b3c9c971dd35dc71911ca8798fb5a5df8f9ecc9
Madhav Arora <146657926+MadhavArora1213@users.noreply.github.com>
2026-06-29 21:39:00 +0530

    Initial commit via Grizon AI

---
+++ backend/controllers/commentsController.js
@@ -0,0 +1,19 @@
+exports.getAllComments = (req, res) => {
+    // Logic to get all comments
+};
+
+exports.createComment = (req, res) => {
+    // Logic to create a new comment
+};
+
+exports.getCommentById = (req, res) => {
+    // Logic to get a comment by ID
+};
+
+exports.updateComment = (req, res) => {
+    // Logic to update a comment by ID
+};
+
+exports.deleteComment = (req, res) => {
+    // Logic to delete a comment by ID
+};
