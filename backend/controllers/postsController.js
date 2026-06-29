048026f66d6cd77ae27347bcd845bf02fc56a35d
Madhav Arora <146657926+MadhavArora1213@users.noreply.github.com>
2026-06-29 21:39:03 +0530

    Initial commit via Grizon AI

---
+++ backend/controllers/postsController.js
@@ -0,0 +1,19 @@
+exports.getAllPosts = (req, res) => {
+    // Logic to get all posts
+};
+
+exports.createPost = (req, res) => {
+    // Logic to create a new post
+};
+
+exports.getPostById = (req, res) => {
+    // Logic to get a post by ID
+};
+
+exports.updatePost = (req, res) => {
+    // Logic to update a post by ID
+};
+
+exports.deletePost = (req, res) => {
+    // Logic to delete a post by ID
+};
