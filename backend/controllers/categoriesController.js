113deaa626663f42749658be3fe542be63c75fc3
Madhav Arora <146657926+MadhavArora1213@users.noreply.github.com>
2026-06-29 21:38:59 +0530

    Initial commit via Grizon AI

---
+++ backend/controllers/categoriesController.js
@@ -0,0 +1,19 @@
+exports.getAllCategories = (req, res) => {
+    // Logic to get all categories
+};
+
+exports.createCategory = (req, res) => {
+    // Logic to create a new category
+};
+
+exports.getCategoryById = (req, res) => {
+    // Logic to get a category by ID
+};
+
+exports.updateCategory = (req, res) => {
+    // Logic to update a category by ID
+};
+
+exports.deleteCategory = (req, res) => {
+    // Logic to delete a category by ID
+};
